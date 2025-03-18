e827209e66a265c1438a118cd7db0b1f06207e29
OwLu0905 <owlu0905@gmail.com>
2025-03-18 23:54:16 +0800

    feat: sidebar & wavesurfer.js

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,186 @@
+<script lang="ts" module>
+	import {
+		AudioWaveform,
+		BookOpen,
+		Bot,
+		ChartPie,
+		Command,
+		Frame,
+		GalleryVerticalEnd,
+		Map,
+		Settings2,
+		SquareTerminal,
+	} from "@lucide/svelte";
+
+	// This is sample data.
+	const data = {
+		user: {
+			name: "Kian",
+			email: "owlu0905@example.com",
+			avatar: "/avatars/shadcn.jpg",
+		},
+		topics: [
+			{
+				name: "Echo",
+				logo: GalleryVerticalEnd,
+				plan: "Enterprise",
+				link: "/echo/1",
+			},
+			{
+				name: "Novel",
+				logo: AudioWaveform,
+				plan: "Startup",
+				link: "/novel/1",
+			},
+			{
+				name: "Home",
+				logo: Command,
+				plan: "Free",
+				link: "/",
+			},
+		],
+		navMain: [
+			{
+				title: "Playground",
+				url: "#",
+				icon: SquareTerminal,
+				isActive: true,
+				items: [
+					{
+						title: "History",
+						url: "#",
+					},
+					{
+						title: "Starred",
+						url: "#",
+					},
+					{
+						title: "Settings",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Models",
+				url: "#",
+				icon: Bot,
+				items: [
+					{
+						title: "Genesis",
+						url: "#",
+					},
+					{
+						title: "Explorer",
+						url: "#",
+					},
+					{
+						title: "Quantum",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Documentation",
+				url: "#",
+				icon: BookOpen,
+				items: [
+					{
+						title: "Introduction",
+						url: "#",
+					},
+					{
+						title: "Get Started",
+						url: "#",
+					},
+					{
+						title: "Tutorials",
+						url: "#",
+					},
+					{
+						title: "Changelog",
+						url: "#",
+					},
+				],
+			},
+			{
+				title: "Settings",
+				url: "#",
+				icon: Settings2,
+				items: [
+					{
+						title: "General",
+						url: "#",
+					},
+					{
+						title: "Team",
+						url: "#",
+					},
+					{
+						title: "Billing",
+						url: "#",
+					},
+					{
+						title: "Limits",
+						url: "#",
+					},
+				],
+			},
+		],
+		projects: [
+			{
+				name: "Design Engineering",
+				url: "#",
+				icon: Frame,
+			},
+			{
+				name: "Sales & Marketing",
+				url: "#",
+				icon: ChartPie,
+			},
+			{
+				name: "Travel",
+				url: "#",
+				icon: Map,
+			},
+		],
+	};
+</script>
+
+<script lang="ts">
+	import { page } from "$app/state";
+	import type { ComponentProps } from "svelte";
+	import NavUser from "$lib/components/nav-user.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import TopicSwitcher from "$lib/components/topic-switcher.svelte";
+	import EchoSidebar from "$lib/components/sidebar/echo-sidebar.svelte";
+	import NovelSidebar from "$lib/components/sidebar/novel-sidebar.svelte";
+	import DefaultSidebar from "$lib/components/sidebar/default-sidebar.svelte";
+
+	let {
+		ref = $bindable(null),
+		collapsible = "icon",
+		...restProps
+	}: ComponentProps<typeof Sidebar.Root> = $props();
+
+	let url = $derived(page.url);
+	let pathname = $derived(url.pathname);
+	let isNovel = $derived(pathname.includes("novel"));
+	let isEcho = $derived(pathname.includes("echo"));
+</script>
+
+<Sidebar.Root bind:ref {collapsible} {...restProps}>
+	<Sidebar.Header>
+		<TopicSwitcher topics={data.topics} />
+	</Sidebar.Header>
+	{#if isEcho}
+		<EchoSidebar />
+	{:else if isNovel}
+		<NovelSidebar />
+	{:else}
+		<DefaultSidebar {data} />
+	{/if}
+	<Sidebar.Footer>
+		<NavUser user={data.user} />
+	</Sidebar.Footer>
+	<Sidebar.Rail />
+</Sidebar.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import Button from "@/components/ui/button/button.svelte";
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { Input } from "$lib/components/ui/input";
 	import { invoke } from "@tauri-apps/api/core";
+	import { toast } from "svelte-sonner";
 
 	let name = $state("");
 	let greetMsg = $state("");
@@ -13,6 +15,28 @@
 </script>
 
 <main class="container">
-	<h1>Welcome to Tauri + Svelte</h1>
-	<Button>123</Button>
+	<h2 class="text-xl text-primary font-bold py-4">Welcome to Tauri + Svelte</h2>
+
+	<form class="row" onsubmit={greet}>
+		<div class="flex gap-4">
+			<Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+			<Button type="submit">Greet</Button>
+		</div>
+	</form>
+	<p>{greetMsg}</p>
+
+	<Button
+		class="my-4"
+		variant="secondary"
+		onclick={() =>
+			toast.success("Event has been created", {
+				description: "Sunday, December 03, 2023 at 9:00 AM",
+				action: {
+					label: "Undo",
+					onClick: () => console.info("Undo"),
+				},
+			})}
+	>
+		Show Toast
+	</Button>
 </main>
