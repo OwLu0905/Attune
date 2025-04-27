b5e154b8f1f9a880e397d37f34f10e1a4e1b52ec
owlu0905 <owlu0905@gmail.com>
2025-04-27 22:31:35 +0800

    WIP: yt videoId embed form

--- src/routes/yt/embed/+page.svelte
+++ src/routes/yt/embed/+page.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
     import { setContext } from "svelte";
 
+    import * as Card from "$lib/components/ui/card/index.js";
+    import Input from "@/components/ui/input/input.svelte";
+    import Label from "@/components/ui/label/label.svelte";
+    import Button from "@/components/ui/button/button.svelte";
+
     import { PlayOrYt, ActionOrYt } from "@/components/youtue";
     import YtDownloadForm from "./yt-download-form.svelte";
-    import * as Card from "$lib/components/ui/card/index.js";
     import { sliderValuesKey } from "@/components/youtue/yt-keys";
 
     import type {
         YouTubeSliderContext,
         TSLIDER_VALUES,
     } from "@/components/youtue/types";
-    import Input from "@/components/ui/input/input.svelte";
-    import Label from "@/components/ui/label/label.svelte";
 
     let sliderValues: TSLIDER_VALUES = $state([0, 100]);
-    let videoUrlId = $state("dQw4w9WgXcQ123");
+
+    let videoUrlId = $state("");
 
     setContext<YouTubeSliderContext>(sliderValuesKey, {
         getSliderValues: () => sliderValues,
@@ -33,8 +36,23 @@
     <div class="col-span-12 self-center justify-self-center @5xl:col-span-6">
         {#if videoUrlId !== "dQw4w9WgXcQ"}
             <div class="w-full">
-                <Label for="videoId">Youtube Url</Label>
-                <Input id="videoId" bind:value={videoUrlId} />
+                <form
+                    class="flex flex-col gap-2"
+                    onsubmit={(e) => {
+                        e.preventDefault();
+                        if (e.currentTarget) {
+                            const data = new FormData(e.currentTarget);
+
+                            console.log(data);
+                        }
+                    }}
+                >
+                    <Label for="videoId">Youtube Url</Label>
+                    <div class="flex gap-2">
+                        <Input id="videoId" name="videoUrl" />
+                        <Button type="submit">Load</Button>
+                    </div>
+                </form>
             </div>
         {:else}
             <PlayOrYt videoId={videoUrlId}>
